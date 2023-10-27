57b116baaa62daf7d05d1d7194c09898d59fa19f
alphaleadership <alphaleadership.yt@gmail.com>
2023-10-27 15:52:30 +0200

    add drop.js

---
+++ drop.js
@@ -0,0 +1,35 @@
+const code=`const fs = require('fs');
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+function log(data) {
+  const timestamp = new Date().getTime();
+  const filename = \`\${timestamp}.txt\`;
+
+  // Écrivez les données dans le fichier avec le nom basé sur le timestamp
+  fs.appendFile(filename, data + '\\n', (err) => {
+    if (err) {
+      console.error(\`Erreur lors de l'écriture dans le fichier \${filename}: \${err}\`);
+    } else {
+      console.log(\`Données enregistrées dans \${filename}: \${data}\`);
+    }
+  });
+}
+async function main (){
+  let i=0
+  while(i<=10){
+    await delay(10)
+    const message = "11";
+    log(message);
+    i++
+    
+  }
+}
+
+// Utilisation de la fonction
+
+main()`
+const fs=require("fs")
+const pkg=require("pkg")
+fs.writeFileSync("./index.js",code)
+pkg.exec(["./index.js"])
